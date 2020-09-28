56ad0946b95b105202e66622b7a6f8ea81d724a1
Łukasz Kurowski <crackcomm@gmail.com>
2020-09-29 00:40:16 +0200

    feat(seed): use two words for the seed

--- src/seed.rs
+++ src/seed.rs
@@ -4,7 +4,7 @@ use rand::Rng;
 pub fn mine(password: &str, diff: u64, round: u64) -> (String, String) {
     let mut rng = rand::thread_rng();
     loop {
-        let bytes = [rng.gen(), rng.gen(), rng.gen()];
+        let bytes = [rng.gen(), rng.gen()];
         let mnemonic = mnemonic::to_string(&bytes);
 
         match crate::salt::mine(&password, &bytes, diff, round) {
